31dd858426a82e54b76185a638242a4a922b5d2e
kristendaya <126866834+kristendaya@users.noreply.github.com>
2023-03-04 16:07:00 +0900

    Update keras11_3_diabet.py
    R2 0.62 이상

--- keras11_3_diabet.py
+++ keras11_3_diabet.py
@@ -17,7 +17,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,
-                                               train_size=0.9,shuffle=True,random_state=5214)
+                                               train_size=0.9,shuffle=True,random_state=72)
                                               
 
 #model
@@ -25,8 +25,8 @@ model=Sequential()
 model.add(Dense(1,input_dim=10))
 model.add(Dense(5))
 model.add(Dense(10))
-model.add(Dense(20))
-model.add(Dense(30))
+model.add(Dense(100))
+model.add(Dense(100))
 model.add(Dense(70))
 model.add(Dense(30))
 model.add(Dense(20))
@@ -35,8 +35,8 @@ model.add(Dense(1))
 
 
 model.compile(loss="mse",optimizer="adam")
-model.fit(x_train,y_train,epochs=500, batch_size=1) 
- 
+model.fit(x_train,y_train,epochs=3000, batch_size=20) 
+
 #평가,예측
 loss=model.evaluate(x_test,y_test)
 print("loss:",loss)
@@ -47,3 +47,5 @@ y_predict=model.predict(x_test)
 from sklearn.metrics import r2_score
 r2= r2_score(y_test,y_predict)
 print('r2스코어:', r2)
+
+#R2 = 0.64 
