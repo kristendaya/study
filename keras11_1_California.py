e3ebea38fc48e99e4cda0b69cc2c043efb57cc65
kristendaya <126866834+kristendaya@users.noreply.github.com>
2023-03-04 16:04:53 +0900

    Update keras11_1_California.py
     R2 0.5 이상

--- keras11_1_California.py
+++ keras11_1_California.py
@@ -17,7 +17,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,
-                                               train_size=0.9,shuffle=True,random_state=1234)
+                                               train_size=0.9,shuffle=True,random_state=12)
                                               
 
 #model
@@ -25,20 +25,20 @@ model=Sequential()
 model.add(Dense(1,input_dim=8))
 model.add(Dense(10))
 model.add(Dense(30))
-model.add(Dense(60))
+model.add(Dense(75))
 model.add(Dense(30))
 model.add(Dense(10))
 model.add(Dense(1))
 
 
 model.compile(loss="mse",optimizer="adam")
-model.fit(x_train,y_train,epochs=50, batch_size=1)
-#혼자 실험=batchsize 높여보니까 확실히 점들이 퍼짐.
+model.fit(x_train,y_train,epochs=120, batch_size=5)
+
  
 #평가,예측
 loss=model.evaluate(x_test,y_test)
 print("loss:",loss)
-y_predict=model.predict(x_test) #훈련안시킨애
+y_predict=model.predict(x_test)
 
 #R2,결정계수 =coefficient of determination
 
@@ -47,4 +47,6 @@ r2= r2_score(y_test,y_predict)
 print('r2스코어:', r2)
 
 # 0.4116824385421938
+#batchsize_15 -> 0.53 / 10->0.52 
+#random = #0.559
 
